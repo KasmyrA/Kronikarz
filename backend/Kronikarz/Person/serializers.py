f2076934eb4e86df6a26c8d7c7c0f908cf70e140
Jakub <kubusiasty@interia.pl>
2025-01-10 19:34:09 +0100

    Poprawki
    Poprawiłem parę błędów związanych. Teraz dodawanie danych do CRUD powinno działać poprawnie. Dodatkowo jest zwracana lista błędów w jsonie, który podaliśmy.

--- backend/Kronikarz/Person/serializers.py
+++ backend/Kronikarz/Person/serializers.py
@@ -35,3 +35,33 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
+
+    def create(self, validated_data):
+        birth_data = validated_data.pop('birth')
+        death_data = validated_data.pop('death', None)
+        surnames_data = validated_data.pop('surnames', [])
+        jobs_data = validated_data.pop('jobs', [])
+        files_data = validated_data.pop('files', [])
+
+        # Tworzenie EventInLife (birth i death)
+        birth = EventInLife.objects.create(**birth_data)
+        death = EventInLife.objects.create(**death_data) if death_data else None
+
+        # Tworzenie osoby
+        person = Person.objects.create(birth=birth, death=death, **validated_data)
+
+        # Tworzenie powiązanych obiektów
+        for surname_data in surnames_data:
+            surname = Surname.objects.create(**surname_data)
+            person.surnames.add(surname)
+
+        for job_data in jobs_data:
+            job = Job.objects.create(**job_data)
+            person.jobs.add(job)
+
+        for file_data in files_data:
+            file = FileInfo.objects.create(**file_data)
+            person.files.add(file)
+
+        return person
+
