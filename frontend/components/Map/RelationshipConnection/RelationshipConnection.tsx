ad3f8fd023d8a1fdefab72ae9408f9d66abb55af
Mateusz Goik <mateuszgoik1@wp.pl>
2025-01-18 18:49:41 +0100

    New relationship connections

--- frontend/components/Map/RelationshipConnection/RelationshipConnection.tsx
+++ frontend/components/Map/RelationshipConnection/RelationshipConnection.tsx
@@ -15,10 +15,11 @@ export function RelationshipConnection({ relationship: rel, people, draggedPerso
   const partner2 = people.find((p) => p.id === rel.partner2)!;
 
   const clickmargin = 25;
+  const conLineDistance = 200;
   const posX = Math.min(partner1.position.x, partner2.position.x) - clickmargin;
   const posY = Math.min(partner1.position.y, partner2.position.y) - clickmargin;
-  const width = Math.abs(partner1.position.x - partner2.position.x) + clickmargin;
-  const height = Math.abs(partner1.position.y - partner2.position.y) + clickmargin;
+  const width = Math.abs(partner1.position.x - partner2.position.x) + clickmargin + conLineDistance;
+  const height = Math.abs(partner1.position.y - partner2.position.y) + clickmargin + conLineDistance;
   
   const viewBox = `${posX + clickmargin / 2} ${posY + clickmargin / 2} ${width} ${height}`;
   const opacity = (rel.partner1 === draggedPerson || rel.partner2 === draggedPerson) ? 0 : 1;
@@ -34,31 +35,30 @@ export function RelationshipConnection({ relationship: rel, people, draggedPerso
   };
 
   const midX = (partner1.position.x + partner2.position.x) / 2;
-  const midY = (partner1.position.y + partner2.position.y) / 2;
-  const clickStrokeWidth = `${clickmargin}px`;
+  const conLineY = Math.max(partner1.position.y, partner2.position.y) + conLineDistance;
 
   const relSymbol = rel.kind === RelationshipKind.SEPARATION ?
     <>
-      <circle className="rel-symbol rel-line" cx={midX} cy={midY} />
-      <line className='rel-line' x1={midX - 6} y1={midY + 6} x2={midX + 6} y2={midY - 6} />
+      <circle className="rel-symbol rel-line" cx={midX} cy={conLineY} />
+      <line className='rel-line' x1={midX - 6} y1={conLineY + 6} x2={midX + 6} y2={conLineY - 6} />
     </> : rel.kind === RelationshipKind.DIVORCE ?
     <>
-      <circle className="rel-symbol rel-line" cx={midX} cy={midY} />
-      <line className='rel-line' x1={midX - 6} y1={midY - 6} x2={midX + 6} y2={midY + 6} />
-      <line className='rel-line' x1={midX - 6} y1={midY + 6} x2={midX + 6} y2={midY - 6} />
+      <circle className="rel-symbol rel-line" cx={midX} cy={conLineY} />
+      <line className='rel-line' x1={midX - 6} y1={conLineY - 6} x2={midX + 6} y2={conLineY + 6} />
+      <line className='rel-line' x1={midX - 6} y1={conLineY + 6} x2={midX + 6} y2={conLineY - 6} />
     </>
     : <></>;
 
   return (
     <svg viewBox={viewBox} style={style} className='rel-connection' xmlns="http://www.w3.org/2000/svg">
       {/* Click margin lines */}
-      <line className='rel-margin' strokeWidth={clickStrokeWidth} onClick={onClick} x1={partner1.position.x} y1={partner1.position.y} x2={midX} y2={partner1.position.y} />
-      <line className='rel-margin' strokeWidth={clickStrokeWidth} onClick={onClick} x1={midX} y1={partner1.position.y} x2={midX} y2={partner2.position.y} />
-      <line className='rel-margin' strokeWidth={clickStrokeWidth} onClick={onClick} x1={midX} y1={partner2.position.y} x2={partner2.position.x} y2={partner2.position.y} />
+      <line className='rel-margin' strokeWidth={clickmargin} onClick={onClick} x1={partner1.position.x} y1={partner1.position.y} x2={partner1.position.x} y2={conLineY} />
+      <line className='rel-margin' strokeWidth={clickmargin} onClick={onClick} x1={partner1.position.x} y1={conLineY} x2={partner2.position.x} y2={conLineY} />
+      <line className='rel-margin' strokeWidth={clickmargin} onClick={onClick} x1={partner2.position.x} y1={conLineY} x2={partner2.position.x} y2={partner2.position.y} />
       {/* Visible lines */}
-      <line className='rel-line' x1={partner1.position.x} y1={partner1.position.y} x2={midX} y2={partner1.position.y} />
-      <line className='rel-line' x1={midX} y1={partner1.position.y} x2={midX} y2={partner2.position.y} />
-      <line className='rel-line' x1={midX} y1={partner2.position.y} x2={partner2.position.x} y2={partner2.position.y} />
+      <line className='rel-line' x1={partner1.position.x} y1={partner1.position.y} x2={partner1.position.x} y2={conLineY} />
+      <line className='rel-line' x1={partner1.position.x} y1={conLineY} x2={partner2.position.x} y2={conLineY} />
+      <line className='rel-line' x1={partner2.position.x} y1={conLineY} x2={partner2.position.x} y2={partner2.position.y} />
       {relSymbol}
     </svg>
   );
