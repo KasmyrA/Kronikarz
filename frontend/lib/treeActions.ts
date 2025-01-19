b6a32392dbe445171c6863f69974b423ff714f67
Mateusz Goik <mateuszgoik1@wp.pl>
2025-01-20 00:23:25 +0100

    Redesign tree list

--- frontend/lib/treeActions.ts
+++ frontend/lib/treeActions.ts
@@ -1,5 +1,26 @@
 import { personToTreePerson } from "./personActions";
 import { Tree } from "./treeInterfaces";
+import { User } from "./userInterfaces";
+
+export function getTreeList(): Promise<Tree[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, name: "Test tree 1", parenthoods: [], people: [], relationships: [] },
+        { id: 2, name: "Test tree 2", parenthoods: [], people: [], relationships: [] },
+        { id: 3, name: "Test tree 3", parenthoods: [], people: [], relationships: [] },
+      ]);
+    }, 1000);
+  });
+}
+
+export async function addTree(treeName: string): Promise<Tree> {
+  return { id: 3, name: treeName, parenthoods: [], people: [], relationships: [] }
+}
+
+export async function deleteTree(treeId: number): Promise<void> {
+  return
+}
 
 export function getTree(id: number): Promise<Tree> {
   return new Promise((resolve) => {
