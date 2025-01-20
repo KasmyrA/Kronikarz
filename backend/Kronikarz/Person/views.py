b765f56c6f3ba5cb664aab8640e83879d6f54034
Jakub <kubusiasty@interia.pl>
2025-01-20 12:47:44 +0100

    .
    .

--- backend/Kronikarz/Person/views.py
+++ backend/Kronikarz/Person/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated
 # Person
 
 @api_view(['GET', 'POST'])
-#@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def persons_list(request, format=None):
 
     if request.method == 'GET':
@@ -27,7 +27,7 @@ def persons_list(request, format=None):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])   
-#@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def person_detail(request, id, format=None):
     try:
         person = Person.objects.get(pk=id)
@@ -49,7 +49,7 @@ def person_detail(request, id, format=None):
 
 
 @api_view(['GET'])
-#@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def persons_by_uid(request, uid, format=None):
     persons = Person.objects.filter(uid=uid)
     if not persons.exists():
