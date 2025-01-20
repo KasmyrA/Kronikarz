8d900b2d8f76fe6f7a33ce26048a6771a76c6a37
Jakub <kubusiasty@interia.pl>
2025-01-20 12:45:10 +0100

    Poprawki błędów
    Poprawiłem działanie klasy Tree i klasy Person.

--- backend/Kronikarz/Tree/urls.py
+++ backend/Kronikarz/Tree/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     # Tree
     path('', views.tree_list),
     path('<int:id>/', views.tree_detail),
+    path('user/<int:uid>/', views.trees_by_uid),
+    # Position
+    path('', views.positions_list),
+    path('<int:id>/', views.position_detail),
+    path('user/<int:uid>/', views.positions_by_uid),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/Kronikarz/Tree/views.py
+++ backend/Kronikarz/Tree/views.py
@@ -2,8 +2,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
-from .models import Tree
-from .serializers import TreeSerializer
+from .models import Tree, Position
+from .serializers import TreeSerializer, PositionSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -57,4 +57,56 @@ def trees_by_uid(request, uid, format=None):
         return Response({'error': 'No trees found with the provided UID'}, status=status.HTTP_404_NOT_FOUND)
     
     serializer = TreeSerializer(trees, many=True)
+    return Response(serializer.data)
+
+
+# Position
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def positions_list(request, format=None):
+
+    if request.method == 'GET':
+        positions = Position.objects.all()
+        serializer = PositionSerializer(positions, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = PositionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])   
+@permission_classes([IsAuthenticated])
+def position_detail(request, id, format=None):
+    try:
+        position = Position.objects.get(pk=id)
+    except Position.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PositionSerializer(position)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PositionSerializer(position, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        position.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def positions_by_uid(request, uid, format=None):
+    positions = Position.objects.filter(uid=uid)
+    if not positions.exists():
+        return Response({'error': 'No positions found with the provided UID'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = PositionSerializer(positions, many=True)
     return Response(serializer.data)
