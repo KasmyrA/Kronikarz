3635d9488bff465688cb9a8a52f27f693a6c2ed8
Kacper <74537153+KasmyrA@users.noreply.github.com>
2025-01-28 02:09:40 +0100

    Minor change

--- frontend/components/ui/ErrorMessage.tsx
+++ frontend/components/ui/ErrorMessage.tsx
@@ -5,7 +5,7 @@ export function ErrorMessage({ message }: { message: string }) {
   
   return (
     <div className="flex items-center gap-2 rounded-md bg-destructive/15 p-3 text-sm text-destructive animate-in ">
-      <XCircle className="h-4 w-4" />
+      <XCircle className="h-6 w-6" />
       <p className="text-red-700">{message}</p>
     </div>
   )
